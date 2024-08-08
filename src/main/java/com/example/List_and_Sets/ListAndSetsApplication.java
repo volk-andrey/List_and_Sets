60449e59e5bb9d3857780493ece8a06bdb861151
Andrey <andrey.volk.1999@gmail.com>
2024-08-08 18:34:25 +1000

    Initial commit

---
+++ src/main/java/com/example/List_and_Sets/ListAndSetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.List_and_Sets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListAndSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListAndSetsApplication.class, args);
+	}
+
+}
